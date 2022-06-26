edf6af77b70dc753f6531dda56d58a2d34bb8a4b
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-06-26 13:47:18 -0300

    fix: project version coming from json file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 import pickle
+import json
 
 app = FastAPI()
 trained_model = None
+project_info = None
 
 class Payload(BaseModel):
   text: str
@@ -14,9 +16,9 @@ def health():
 
 @app.post("/api/american")
 def is_american_tweet(response: Response, payload: Payload):
-  if trained_model == None:
+  if trained_model == None or project_info == None:
     response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
-    return { "message": "model not loaded yet" }
+    return { "message": "server not loaded yet" }
   
   X_new_count = trained_model["count_vect"].transform([payload.text])
   X_tfidf = trained_model["tfidf_transformer"].transform(X_new_count)
@@ -24,7 +26,7 @@ def is_american_tweet(response: Response, payload: Payload):
 
   return {
     "is_american": predicted.tolist()[0],
-    "version": trained_model["version"],
+    "version": project_info["version"],
     "model_date": trained_model["model_date"]
   }
 
@@ -37,4 +39,14 @@ def load_model():
   except:
     print("Could not load trained model!")
 
+def load_info():
+  global project_info
+  try:
+    file = open("./info.json", "r")
+    project_info = json.loads(file.read())
+    file.close()
+  except:
+    print("Could not load server info!")
+
 load_model()
+load_info()
